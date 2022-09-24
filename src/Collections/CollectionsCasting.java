3ec4f9f50c47f3cd36999d84949b4fa3033a0bcf
MariaPolovinkina <maria.kruchinina@gmail.com>
2022-09-24 13:26:10 -0700

    formatListToMap method is added

--- src/Collections/CollectionsCasting.java
+++ src/Collections/CollectionsCasting.java
@@ -1,9 +1,21 @@
 package Collections;
 
+import java.util.List;
+import java.util.Map;
+
 public class CollectionsCasting {
+	
+	public Map<Integer, Integer> formatListToMap(List<Integer> sourceList) {
+		
+		//Add your own return
+		return null;
+	}
 
 	public void calculate() {		
-	//It will be the next task	
+	
+	//Generate 100 random integer values between [1...10]
+		
+	//Call the formatListToMap(...) method
 			
 	}
 }
